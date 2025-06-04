e6e46087e37e3a07906a058c836bc13784168e27
Zaziben <astraljv@gmail.com>
2025-06-04 00:54:34 -0400

    june3

--- infra/main.tf
+++ infra/main.tf
@@ -15,21 +15,15 @@ module "eks" {
   # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
-  cluster_compute_config = {
-    enabled    = true
-    node_pools = ["general-purpose"]
-  }
-
   eks_managed_node_group_defaults = {
     ami_type = "AL2023_x86_64_STANDARD"
   }
 
   vpc_id     = var.vpc_id  
-    subnet_ids = ["subnet-07e4939eae8fcd6bb", "subnet-04181d7c9e2cc0f09", ]
-    control_plane_subnet_ids = ["subnet-07c91559f4c8535ae", "subnet-09d75a39c650da09c" ]
+    subnet_ids = ["subnet-048916d83b3d6dcf1", "subnet-07e4939eae8fcd6bb", "subnet-04181d7c9e2cc0f09", ]
 
-    eks_managed_node_groups = {
 
+  eks_managed_node_groups = {  
     one = {
       name = "node-group-1"
 
@@ -39,44 +33,12 @@ module "eks" {
       max_size     = 2
       desired_size = 1
     }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.medium"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
   }
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
-    
    
   cluster_addons = {
-    coredns                = {}
+    metrics_server = {}
     eks-pod-identity-agent = {}
-    kube-proxy             = {}
-    aws-ebs-csi-driver = {
-      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-    }
+    aws-ebs-csi-driver = {}
   }
 }
 
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
-
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "5.39.0"
-
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
